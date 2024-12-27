eb07809c55bd1dab886e7f68e88ad4f469e63438
Felipe Lalanne <felipe@balena.io>
2024-12-27 17:42:27 -0300

    Remove non-iterable version of Blueprint.reproduce
    An array result is easily expanded from the iterator by using the spread
    operator. The non-iterable version is used only on the supervisor and
    will be replaced soon.
    
    Change-type: minor

--- lib/index.ts
+++ lib/index.ts
@@ -27,25 +27,49 @@ export {
 	parseCardinality,
 };
 
+/**
+ * @summary Reproduce the query in a universe and return as an iterable
+ * @function
+ * @public
+ * @memberof module:contrato
+ * @name module:contrato.query
+ * @description
+ * This method will generate a set of contexts that consist of
+ * every possible valid combination that matches the blueprint
+ * layout. It uses depth first search to calculate the product of
+ * contract combinations and returns the results as an iterable.
+ * This allows to reduce the memory usage when dealing with a large
+ * universe of contracts.
+ *
+ * @param universe - composite contract
+ * @param layout - an object describing the query using a set of cardinality operators
+ * @param skeleton - contract skeleton for the returned contexts
+ * @returns an iterable over the valid contexts
+ *
+ ** @example
+ * import {query, Contract} from 'contrato';
+ *
+ * const contract = new Contract({ ... })
+ * contract.addChildren([ ... ])
+ *
+ * const contexts = query(contract, {
+ *			'hw.device-type': 1,
+ *			'arch.sw': {
+ *				cardinality: 1,
+ *				filter: { type: 'object', properties: { slug: { const: 'armv7hf' } } },
+ *			},
+ *		});
+ *
+ * for (const context of contexts) {
+ *		console.log(context.toJSON());
+ * }
+ */
 export function query(
 	universe: Contract,
 	layout: BlueprintLayout,
 	skeleton: object,
-	asIterable: true,
-): IterableIterator<Contract>;
-export function query(
-	universe: Contract,
-	layout: BlueprintLayout,
-	skeleton: object,
-	asIterable?: false,
-): Contract[];
-export function query(
-	universe: Contract,
-	layout: BlueprintLayout,
-	skeleton: object,
-	asIterable = false,
-): IterableIterator<Contract> | Contract[] {
-	return new Blueprint(layout, skeleton).reproduce(universe, asIterable);
+): IterableIterator<Contract> {
+	return new Blueprint(layout, skeleton).reproduce(universe);
 }
 
 export const sequence = (
